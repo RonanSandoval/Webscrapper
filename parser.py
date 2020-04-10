220aa30bafa9f3fd9d50d2d4098171d1895d855c
Ronan Sandoval <ronanchr@ualberta.ca>
2020-04-09 21:40:14 -0600

    Obtains categories, but only first phrases

--- parser.py
+++ parser.py
@@ -5,8 +5,28 @@ from bs4 import BeautifulSoup
 with open("Algonquian Linguistics Atlas.html", "r") as f:
     content = f.read()
 soup = BeautifulSoup(content ,'html.parser')
-print(soup.prettify())
 
-categories = soup.find(id = "categoryList")
+soupCategories = soup.find(id = "categoryList")
+
+# categories are obtained
+categories = []
+
+for item in soupCategories.contents[1]:
+    categories.append(item.getText())
 
 print(categories)
+
+# phrases are obtained
+soupMeaninglist = soup.find_all(id = "meaningList")
+
+meaningList = []
+
+for item in soupMeaninglist[1].contents[2]:
+    meaningList.append(item.getText())
+
+print(meaningList)
+
+
+
+
+
